1eb6a8b8b223a6b7b3e96b2a2602245a0fbfdd95
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-21 16:01:15 -0700

    Added color changer. Project is now feature complete

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let color = DEFAULT_COLOR;
 
 function loadGrid(grid_size) {
     const container = document.querySelector('#container');
-
+    // Creates all divs based on current grid size
     for (let i = 0; i < grid_size; i++) {
         const content = document.createElement('div');
         content.classList.add('child');
@@ -16,6 +16,7 @@ function loadGrid(grid_size) {
         for (let j = 0; j < grid_size; j++) {
             const new_content = document.createElement('div');
             new_content.classList.add('child1');
+            // Sets up listeners to know when conditions are met to draw
             new_content.addEventListener('mousedown', event => { mouseDown = true; })
             new_content.addEventListener('mouseup', event => { mouseDown = false; })
             new_content.addEventListener('mouseover', highlight);
@@ -24,6 +25,7 @@ function loadGrid(grid_size) {
     }
 }
 
+// If eraser is toggled, will change div color to white instead of selected color
 function toggleEraser() {
     eraser = !eraser;
     if (eraser) {
@@ -31,21 +33,34 @@ function toggleEraser() {
         color = '#fefefe';
     } else {
         document.getElementById('eraser').innerHTML = 'Eraser Off';
-        color = DEFAULT_COLOR;
+        color = document.getElementById('colorpicker').value;
     }
 }
+
+// Draws on divs
 function highlight(e) {
     if (e.type === 'mouseover' && mouseDown)
     e.target.style.backgroundColor = color;
 }
 
+// Grabs new color info and updates color
+function changeColor(new_color) {
+    color = new_color;
+    document.getElementById('color-button').style.color = color;
+}
+
+// Resets everything to default
 function reset(size) {
-    const parent = document.getElementById('container');
-    parent.innerHTML = '';
+    if (size > 100 || size < 2) {
+        alert('Grid cannot be more than 100x100 and less than 2x2');
+        size = 16;
+    }
+    document.getElementById('container').innerHTML = '';
+    document.getElementById('grid-size').innerHTML = size + ' x ' + size;
+    changeColor(DEFAULT_COLOR);
     loadGrid(size);
 }
 
 window.onload = () => {
     loadGrid(DEFAULT_SIZE);
 }
-
