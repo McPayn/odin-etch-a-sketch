0acae2f79946a797f6b3d94ec27b16666da24db3
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-21 14:18:43 -0700

    div background color changes on mouseclick Added reset button that clears grid

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const DEFAULT_COLOR = '#000000'
+
 function loadGrid() {
     const container = document.querySelector('#container');
 
@@ -8,13 +10,22 @@ function loadGrid() {
         for (let j = 0; j < 16; j++) {
             const new_content = document.createElement('div');
             new_content.classList.add('child1');
+            new_content.addEventListener('mousedown', highlight);
             content.appendChild(new_content);
         }
     }
-
 }
 
+function highlight(e) {
+    e.target.style.backgroundColor = DEFAULT_COLOR;
+}
 
 window.onload = () => {
+    loadGrid();
+}
+
+function reset() {
+    const parent = document.getElementById('container');
+    parent.innerHTML = '';
     loadGrid();
 }
