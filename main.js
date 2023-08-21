7f579bb656349af5b11ff2cb6b55cf7508d378f7
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-21 14:38:29 -0700

    Added drag and click functionality, reset grid button, grid size button

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 const DEFAULT_COLOR = '#000000'
+const DEFAULT_SIZE = 16;
 
-function loadGrid() {
+
+let mouseDown = false;
+
+function loadGrid(grid_size) {
     const container = document.querySelector('#container');
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < grid_size; i++) {
         const content = document.createElement('div');
         content.classList.add('child');
         container.appendChild(content);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < grid_size; j++) {
             const new_content = document.createElement('div');
             new_content.classList.add('child1');
-            new_content.addEventListener('mousedown', highlight);
+            new_content.addEventListener('mousedown', event => { mouseDown = true; })
+            new_content.addEventListener('mouseup', event => { mouseDown = false; })
+            new_content.addEventListener('mouseover', highlight);
             content.appendChild(new_content);
         }
     }
 }
 
 function highlight(e) {
+    if (e.type === 'mouseover' && mouseDown)
     e.target.style.backgroundColor = DEFAULT_COLOR;
 }
 
-window.onload = () => {
-    loadGrid();
-}
-
-function reset() {
+function reset(size) {
     const parent = document.getElementById('container');
     parent.innerHTML = '';
-    loadGrid();
+    loadGrid(size);
 }
+
+window.onload = () => {
+    loadGrid(DEFAULT_SIZE);
+}
+
